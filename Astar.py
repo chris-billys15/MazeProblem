1280f509e3274d514d6f6bcf1e731223685dc76b
chris-billys15 <42112415+chris-billys15@users.noreply.github.com>
2019-03-31 12:43:12 +0700

    FIXFIXFIX

--- Astar.py
+++ Astar.py
@@ -6,214 +6,227 @@ visitedPoint = [] # Point yang dilalui atau diexpand
 AStarQueue = [] #Queue of Node
 
 class Node:
-    def __init__(self, cost, point ,path):
-        self.cost = cost
-        self.point = point
-        self.path = path
-    def addPath(self):
-        pathTemp = []
-        pathTemp = copy.deepcopy(self.path)
-        pathTemp.append(self.point)
-    
+	def __init__(self, cost, point ,path):
+		self.cost = cost
+		self.point = point
+		self.path = path
+	def addPath(self):
+		pathTemp = []
+		pathTemp = copy.deepcopy(self.path)
+		pathTemp.append(self.point)
+	
 def printMatrix(M):
-    for i in range(len(M)):
-        for j in range (len(M[i])):
-            if(j == len(M[i]) -1):
-                if(M[i][j] == 1):
-                    print("\033[1;36;46m",M[i][j] ,"\033[0m")
-                elif(M[i][j] == 0):
-                    print("\033[1;37;47m",M[i][j] ,"\033[0m")
-                elif(M[i][j] == 4):
-                    print("\033[1;37;40m",M[i][j] ,"\033[0m")
-            else:
-                if(M[i][j] == 1):
-                    print("\033[1;36;46m",M[i][j] ,"\033[0m",end ='')
-                elif(M[i][j] == 0):
-                    print("\033[1;37;47m",M[i][j] ,"\033[0m",end ='')
-                elif(M[i][j] == 4):
-                    print("\033[1;37;40m",M[i][j] ,"\033[0m",end ='')
-                # print(M[i][j],end ='')
+	for i in range(len(M)):
+		for j in range (len(M[i])):
+			if(j == len(M[i]) -1):
+				if(M[i][j] == '1'):
+					print("\033[1;37;47m",M[i][j] ,"\033[0m")
+				elif(M[i][j] == '0'):
+					print("\033[1;38;48m",M[i][j] ,"\033[0m")
+				elif(M[i][j] == '#'):
+					print("\033[5;37;46m",M[i][j] ,"\033[0m")
+			else:
+				if(M[i][j] == '1'):
+					print("\033[1;37;47m",M[i][j] ,"\033[0m",end ='')
+				elif(M[i][j] == '0'):
+					print("\033[1;38;48m",M[i][j] ,"\033[0m",end ='')
+				elif(M[i][j] == '#'):
+					print("\033[5;37;46m",M[i][j] ,"\033[0m",end ='')
+				# print(M[i][j],end ='')
 
 
 def manhattan(current, goal):
-    #Return the heuristic distance from current position to goal.
-    # print("manhattan :" ,abs(current[0] - goal[0]) + abs(current[1] - goal[1]))
-    return abs(current[0] - goal[0]) + abs(current[1] - goal[1])
+	#Return the heuristic distance from current position to goal.
+	# print("manhattan :" ,abs(current[0] - goal[0]) + abs(current[1] - goal[1]))
+	return abs(current[0] - goal[0]) + abs(current[1] - goal[1])
 
 def cost(curNode,goalNode,costToNode):
-    gn = curNode.cost - manhattan(curNode.point,goalNode.point) + costToNode -1
-    cost = gn + manhattan(curNode.point,goalNode.point)
-    return cost 
+	gn = curNode.cost - manhattan(curNode.point,goalNode.point) + costToNode -1
+	cost = gn + manhattan(curNode.point,goalNode.point)
+	return cost 
 
 def readFile(filename):
-    contentTemp = []
-    with open(filename) as f:
-        contentTemp = f.readlines()
-    # you may also want to remove whitespace characters like `\n` at the end of each line
-    contentTemp = [x.strip() for x in contentTemp]
-    maps = []
-    for i in range(len(contentTemp)):
-        maps.append([])
-        for j in range (len(contentTemp[0])):
-            if(contentTemp[i][j] == '1'):
-                # maps[i][j] = '1'
-                maps[i].append('1')
-            elif(contentTemp[i][j] == '0'):
-                # maps[i][j] = '0'
-                maps[i].append('0')
-    return maps
+	contentTemp = []
+	with open(filename) as f:
+		contentTemp = f.readlines()
+	# you may also want to remove whitespace characters like `\n` at the end of each line
+	contentTemp = [x.strip() for x in contentTemp]
+	maps = []
+	for i in range(len(contentTemp)):
+		maps.append([])
+		for j in range (len(contentTemp[0])):
+			if(contentTemp[i][j] == '1'):
+				# maps[i][j] = '1'
+				maps[i].append('1')
+			elif(contentTemp[i][j] == '0'):
+				# maps[i][j] = '0'
+				maps[i].append('0')
+	return maps
 
 def manyPath(curPos):
-    pathChoice = 0
-    if(curPos[1] == 0):
-        if(content[curPos[0]][curPos[1]+1]):
-            pathChoice += 1
-        if(content[curPos[0]+1][curPos[1]]):
-            pathChoice += 1
-        if(content[curPos[0]-1][curPos[1]]):
-            pathChoice += 1
-    elif(curPos[1] == len(content) - 1):
-        if(content[curPos[0]][curPos[1]-1]):
-            pathChoice += 1
-        if(content[curPos[0]+1][curPos[1]]):
-            pathChoice += 1
-        if(content[curPos[0]-1][curPos[1]]):
-            pathChoice += 1
-    elif(curPos[1] > 0 and curPos[1] < len(content)):
-        if(content[curPos[0]][curPos[1]+1]):
-            pathChoice += 1
-        if(content[curPos[0]][curPos[1]-1]):
-            pathChoice += 1
-        if(content[curPos[0]+1][curPos[1]]):
-            pathChoice += 1
-        if(content[curPos[0]-1][curPos[1]]):
-            pathChoice += 1
-    return pathChoice
+	pathChoice = 0
+	if(curPos[1] == 0):
+		if(content[curPos[0]][curPos[1]+1]):
+			pathChoice += 1
+		if(content[curPos[0]+1][curPos[1]]):
+			pathChoice += 1
+		if(content[curPos[0]-1][curPos[1]]):
+			pathChoice += 1
+	elif(curPos[1] == len(content) - 1):
+		if(content[curPos[0]][curPos[1]-1]):
+			pathChoice += 1
+		if(content[curPos[0]+1][curPos[1]]):
+			pathChoice += 1
+		if(content[curPos[0]-1][curPos[1]]):
+			pathChoice += 1
+	elif(curPos[1] > 0 and curPos[1] < len(content)):
+		if(content[curPos[0]][curPos[1]+1]):
+			pathChoice += 1
+		if(content[curPos[0]][curPos[1]-1]):
+			pathChoice += 1
+		if(content[curPos[0]+1][curPos[1]]):
+			pathChoice += 1
+		if(content[curPos[0]-1][curPos[1]]):
+			pathChoice += 1
+	return pathChoice
 
 def searchPoint(pointB, pointK, list1):
-    found = False
-    i = 0
-    while(i < len(list1) and not(found)):
-        if(list1[i][0] == pointB and list1[i][1] == pointK):
-            found = True
-        else:
-            i += 1
-    if(found):
-        return i
-    else:
-        return -1
+	found = False
+	i = 0
+	while(i < len(list1) and not(found)):
+		if(list1[i][0] == pointB and list1[i][1] == pointK):
+			found = True
+		else:
+			i += 1
+	if(found):
+		return i
+	else:
+		return -1
 
+def addPrio(Queue,item):
+	Queue.append(item)
+	idx = 0
+	while(Queue[idx].cost < item.cost):
+		idx+=1
+
+	j = len(Queue) -1
+	while(j > idx):
+		Queue[j] = Queue[j-1]
+		j -= 1
+	Queue[j] = item
+	
 def expand(curNode,goalNode):
-    curPos = copy.deepcopy(curNode.point)
-    # print(curPos)
-    visitedPointFromNode = [] # Point yang di lalui dari Node A ke B
-    visitedPoint.append(curPos)
-    visitedPointFromNode.append(curPos)
+	curPos = copy.deepcopy(curNode.point)
+	# print(curPos)
+	visitedPointFromNode = [] # Point yang di lalui dari Node A ke B
+	visitedPoint.append(curPos)
+	visitedPointFromNode.append(curPos)
 
-    #expand ke kanan
-    if(content[curPos[0]][curPos[1]+1] == '0' and searchPoint(curPos[0], curPos[1]+1, visitedPoint) == -1):
-        curPosRight = copy.deepcopy(curPos)
-        curPosRight[1] += 1
-        AStarQueue.append(buildNode(curNode,goalNode,curPosRight))
-        AStarQueue.sort(key=lambda cost: cost,reverse=False)
-        # print(AStarQueue)
+	#expand ke kanan
+	if(content[curPos[0]][curPos[1]+1] == '0' and searchPoint(curPos[0], curPos[1]+1, visitedPoint) == -1):
+		curPosRight = copy.deepcopy(curPos)
+		curPosRight[1] += 1
+		addPrio(AStarQueue,buildNode(curNode,goalNode,curPosRight))
 
-    #expand ke kiri
-    if(content[curPos[0]][curPos[1]-1] == '0' and searchPoint(curPos[0], curPos[1]-1, visitedPoint) == -1):
-        curPosLeft = copy.deepcopy(curPos)
-        curPosLeft[1] -= 1
-        AStarQueue.append(buildNode(curNode,goalNode,curPosLeft))
-        AStarQueue.sort(key= lambda cost: cost, reverse=False)
+	#expand ke kiri
+	if(content[curPos[0]][curPos[1]-1] == '0' and searchPoint(curPos[0], curPos[1]-1, visitedPoint) == -1):
+		curPosLeft = copy.deepcopy(curPos)
+		curPosLeft[1] -= 1
+		addPrio(AStarQueue,buildNode(curNode,goalNode,curPosLeft))
 
-    #expand ke bawah
-    if(content[curPos[0]+1][curPos[1]] == '0' and searchPoint(curPos[0]+1, curPos[1], visitedPoint) == -1):
-        curPosDown = copy.deepcopy(curPos)
-        curPosDown[0] += 1
-        AStarQueue.append(buildNode(curNode,goalNode,curPosDown))
-        AStarQueue.sort(key=lambda cost: cost, reverse=False)
+	#expand ke bawah
+	if(content[curPos[0]+1][curPos[1]] == '0' and searchPoint(curPos[0]+1, curPos[1], visitedPoint) == -1):
+		curPosDown = copy.deepcopy(curPos)
+		curPosDown[0] += 1
+		addPrio(AStarQueue,buildNode(curNode,goalNode,curPosDown))
 
-    #expand ke atas
-    if(content[curPos[0]-1][curPos[1]] == '0' and searchPoint(curPos[0]-1, curPos[1], visitedPoint) == -1):
-        curPosUp = copy.deepcopy(curPos)
-        curPosUp[0] -= 1
-        AStarQueue.append(buildNode(curNode,goalNode,curPosUp))
-        AStarQueue.sort(key=lambda cost: cost, reverse=False)
+	#expand ke atas
+	if(content[curPos[0]-1][curPos[1]] == '0' and searchPoint(curPos[0]-1, curPos[1], visitedPoint) == -1):
+		curPosUp = copy.deepcopy(curPos)
+		curPosUp[0] -= 1
+		addPrio(AStarQueue,buildNode(curNode,goalNode,curPosUp))
 
 def buildNode(curNode,goalNode,curPos):
-    visitedPointFromNode = []
-    visitedPointFromNode.append(curPos)
-    visitedPoint.append(curPos)
-    if(curPos[1] == 0):
-        if(content[curPos[0]][curPos[1]+1] == '0' and searchPoint(curPos[0], curPos[1]+1, visitedPoint) == -1):
-            curPos[1] += 1
-            visitedPoint.append(curPos)
-            visitedPointFromNode.append(curPos)
-    while(manyPath(curPos) < 3 and manyPath(curPos) != 1):
-        if(curPos[1] == 0):
-            if(content[curPos[0]][curPos[1]+1] == '0' and searchPoint(curPos[0], curPos[1]+1, visitedPoint) == -1):
-                curPos[1] += 1
-                visitedPoint.append(curPos)
-                visitedPointFromNode.append(curPos)
+	visitedPointFromNode = []
+	visitedPointFromNode.append(curPos)
+	visitedPoint.append(curPos)
+	if(curPos[1] == 0):
+		if(content[curPos[0]][curPos[1]+1] == '0' and searchPoint(curPos[0], curPos[1]+1, visitedPoint) == -1):
+			curPos[1] += 1
+			visitedPoint.append(curPos)
+			visitedPointFromNode.append(curPos)
+	while(manyPath(curPos) < 3 and manyPath(curPos) != 1):
+		if(curPos[1] == 0):
+			if(content[curPos[0]][curPos[1]+1] == '0' and searchPoint(curPos[0], curPos[1]+1, visitedPoint) == -1):
+				curPos[1] += 1
+				visitedPoint.append(curPos)
+				visitedPointFromNode.append(curPos)
 
-        elif(curPos[1] > 0 and curPos[1] < len(content)):
-            if(content[curPos[0]][curPos[1]+1] == '0' and searchPoint(curPos[0], curPos[1]+1, visitedPoint) == -1):
-                curPos[1] += 1
-                visitedPoint.append(curPos)
-                visitedPointFromNode.append(curPos)
+		elif(curPos[1] > 0 and curPos[1] < len(content)):
+			if(content[curPos[0]][curPos[1]+1] == '0' and searchPoint(curPos[0], curPos[1]+1, visitedPoint) == -1):
+				curPos[1] += 1
+				visitedPoint.append(curPos)
+				visitedPointFromNode.append(curPos)
 
-            if(content[curPos[0]][curPos[1]-1] == '0' and searchPoint(curPos[0], curPos[1]-1, visitedPoint) == -1):
-                curPos[1] -= 1
-                visitedPoint.append(curPos)
-                visitedPointFromNode.append(curPos)
+			if(content[curPos[0]][curPos[1]-1] == '0' and searchPoint(curPos[0], curPos[1]-1, visitedPoint) == -1):
+				curPos[1] -= 1
+				visitedPoint.append(curPos)
+				visitedPointFromNode.append(curPos)
 
-            if(content[curPos[0]+1][curPos[1]] == '0' and searchPoint(curPos[0]+1, curPos[1], visitedPoint) == -1):
-                curPos[0] += 1
-                visitedPoint.append(curPos)
-                visitedPointFromNode.append(curPos)
+			if(content[curPos[0]+1][curPos[1]] == '0' and searchPoint(curPos[0]+1, curPos[1], visitedPoint) == -1):
+				curPos[0] += 1
+				visitedPoint.append(curPos)
+				visitedPointFromNode.append(curPos)
 
-            if(content[curPos[0]-1][curPos[1]] == '0' and searchPoint(curPos[0]-1, curPos[1], visitedPoint) == -1):
-                curPos[0] -= 1
-                visitedPoint.append(curPos)
-                visitedPointFromNode.append(curPos)
+			if(content[curPos[0]-1][curPos[1]] == '0' and searchPoint(curPos[0]-1, curPos[1], visitedPoint) == -1):
+				curPos[0] -= 1
+				visitedPoint.append(curPos)
+				visitedPointFromNode.append(curPos)
 
-    return Node(cost(curNode, goalNode, len(visitedPointFromNode)),curPos, curNode.path + visitedPointFromNode)
+	return Node(cost(curNode, goalNode, len(visitedPointFromNode)),curPos, curNode.path + visitedPointFromNode)
 
 def AStar(curNode, goalNode):
-    curTemp = copy.deepcopy(curNode)
-    goalTemp = copy.deepcopy(goalNode)
-    i = 0
-    while(curTemp.point[1] < len(content)-1):
-        print(i)
-        expand(curTemp,goalTemp)
-        curTemp = AStarQueue.pop(0)
-        i +=1
-    goalNode = copy.deepcopy(curTemp)
-    print(goalNode.path)
+	curTemp = copy.deepcopy(curNode)
+	goalTemp = copy.deepcopy(goalNode)
+	while(curTemp.point[1] < len(content)-1):
+		expand(curTemp,goalTemp)
+		curTemp = AStarQueue.pop(0)
+	goalNode = copy.deepcopy(curTemp)
+	# print(goalNode.path)
+	return goalNode
 
-def mainAStar():
-    global content, visitedPoint, AStarQueue
-    content = readFile("input.txt")
-    print(content)
+# def init():
+# 	content = [] # Matrix of char
+# 	visitedPoint = [] # Point yang dilalui atau diexpand
+# 	AStarQueue = [] #Queue of Node
 
-    goal = 0
-    found = False
-    while(goal < len(content) and not(found)):
-        if(content[goal][len(content)-1] == '0'):
-            found = True
-        else:
-            goal += 1
+def mainAStar(filename):
+	global content, visitedPoint, AStarQueue
+	content = readFile(filename)
+	# print(content)
 
-    goalNode = Node(0,[goal,len(content)-1],[])
+	goal = 0
+	found = False
+	while(goal < len(content) and not(found)):
+		if(content[goal][len(content)-1] == '0'):
+			found = True
+		else:
+			goal += 1
 
-    start = 0
-    found = False
-    while(start < len(content) and not(found)):
-        if(content[start][0] == '0'):
-            found = True
-        else:
-            start += 1
-    curNode = Node(0 + manhattan([start,0],[goal,len(content)-1]),[start,0],[])
+	goalNode = Node(0,[goal,len(content)-1],[])
 
-    AStar(curNode, goalNode)
+	start = 0
+	found = False
+	while(start < len(content) and not(found)):
+		if(content[start][0] == '0'):
+			found = True
+		else:
+			start += 1
+	curNode = Node(0 + manhattan([start,0],[goal,len(content)-1]),[start,0],[[start,0]])
+	curNode = AStar(curNode, goalNode)
 
-mainAStar()
+	i = 0
+	while(i < len(curNode.path)):
+		content[curNode.path[i][0]][curNode.path[i][1]] = '#'
+		i += 1
+	printMatrix(content)
---
+++ mainProgram.py
@@ -0,0 +1,35 @@
+import BFS2
+import Astar
+
+print(".-. .-')                .-') _    ('-.     _  .-')                             ('-.    .-. .-')           _  .-')               .-') _  .-') _    ('-. .-.")
+print("\  ( OO )              ( OO ) )  ( OO ).-.( \( -O )                           ( OO ).-.\  ( OO )         ( \( -O )             ( OO ) )(  OO) )  ( OO )  /")
+print(" ;-----.\   ,-.-') ,--./ ,--,'   / . --. / ,------.   ,--.   ,--.  ,--.       / . --. / ;-----.\   ,-.-') ,------.  ,-.-') ,--./ ,--,' /     '._ ,--. ,--.")
+print(" | .-.  |   |  |OO)|   \ |  |\   | \-.  \  |   /`. '   \  `.'  /   |  |.-')   | \-.  \  | .-.  |   |  |OO)|   /`. ' |  |OO)|   \ |  |\ |'--...__)|  | |  |")
+print(" | '-' /_)  |  |  \|    \|  | ).-'-'  |  | |  /  | | .-')     /    |  | OO ).-'-'  |  | | '-' /_)  |  |  \|  /  | | |  |  \|    \|  | )'--.  .--'|   .|  |")
+print(" | .-. `.   |  |(_/|  .     |/  \| |_.'  | |  |_.' |(OO  \   /     |  |`-' | \| |_.'  | | .-. `.   |  |(_/|  |_.' | |  |(_/|  .     |/    |  |   |       |")
+print(" | |  \  | ,|  |_.'|  |\    |    |  .-.  | |  .  '.' |   /  /\_   (|  '---.'  |  .-.  | | |  \  | ,|  |_.'|  .  '.',|  |_.'|  |\    |     |  |   |  .-.  |")
+print(" | '--'  /(_|  |   |  | \   |    |  | |  | |  |\  \  `-./  /.__)   |      |   |  | |  | | '--'  /(_|  |   |  |\  \(_|  |   |  | \   |     |  |   |  | |  |")
+print(" `------'   `--'   `--'  `--'    `--' `--' `--' '--'   `--'        `------'   `--' `--' `------'   `--'   `--' '--' `--'   `--'  `--'     `--'   `--' `--'")
+
+print("WELCOME TO BINARY LABIRINTH!")
+print("Choose your Path!")
+valid  = True
+while (valid):
+	print("List of Choices :")
+	print("1. BFS")
+	print("2. A Star \n")
+	choice = input(">> ")
+	
+	if (choice == "BFS" or choice == "1"):
+		filename = input("FileName : ")
+		BFS2.mainBFS(filename)
+	elif (choice == "A Star" or choice == "2"):
+		filename = input("FileName : ")
+		Astar.mainAStar(filename)
+	elif(choice == "exit" or choice == "Exit"):
+		valid = False
+	else:
+		print("Input is not Valid\n")
+		valid = False
+
+
